3aeb51565e7565280cee9231de5003d65b237986
ConyLuLu <v830121v@gmail.com>
2015-08-30 19:23:20 +0800

    新增floating-btn
    floating-btn只能hovrer
    click有問題

--- js/app.js
+++ js/app.js
@@ -39,7 +39,6 @@ angular.module('myTrip', ['ionic'])
   }
 
   $scope.toMap = function() {
-    console.log("In app.js toMap function");
     $state.go('map');
   }
 
@@ -47,3 +46,72 @@ angular.module('myTrip', ['ionic'])
     $ionicHistory.goBack();
   }
 });
+
+var demo = angular.module('demo', ['ng-mfb']);
+
+  demo.controller('myCtrl', Ctrl);
+
+  function Ctrl(defaultValues, $window){
+    var vm = this;
+
+    vm.positions = defaultValues.positions;
+    vm.effects = defaultValues.effects;
+    vm.methods = defaultValues.methods;
+    vm.actions = defaultValues.actions;
+
+    vm.menuState = 'closed';
+    vm.loc = loc;
+    vm.setMainAction = setMainAction;
+    vm.mainAction = mainAction;
+
+    vm.chosen = {
+      effect : 'zoomin',
+      position : 'br',
+      method : 'click',
+      action : 'fire'
+    };
+
+    vm.buttons = [{
+      label: 'View on Github',
+      icon: 'ion-social-github',
+      href: 'https://github.com/nobitagit/ng-material-floating-button/'
+    },{
+      label: 'Follow me on Github',
+      icon: 'ion-social-octocat',
+      href: 'https://github.com/nobitagit'
+    },{
+      label: 'Share on Twitter',
+      icon: 'ion-social-twitter',
+      href: 'http://twitter.com/share?text=Amazing material floating action button directive!&url=http://nobitagit.github.io/ng-material-floating-button/&hashtags=angular,material,design,button'
+    }];
+
+    function loc(href) {
+      $window.location.href = href;
+    }
+
+    function mainAction() {
+      //console.log('Firing Main Action!');
+    }
+
+    function setMainAction() {
+      if(vm.chosen.action === 'fire') {
+        vm.mainAction = mainAction;
+      } else {
+        vm.mainAction = null;
+      }
+    }
+  }
+
+  Ctrl.prototype.hovered = function() {
+    // toggle something on hover.
+  };
+
+  Ctrl.prototype.toggle = function() {
+    this.menuState = this.menuState === 'closed' ? 'open' : 'closed';
+  };
+
+  Ctrl.prototype.closeMenu = function() {
+    this.menuState = 'closed';
+  };
+
+  Ctrl.$inject = ['defaultValues', '$window'];
